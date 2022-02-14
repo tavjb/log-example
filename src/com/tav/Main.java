43b7e03ef16064fcd832568f63511b442498c79e
Tavh <44731477+Tavh@users.noreply.github.com>
2022-02-14 14:33:08 +0200

    git init

--- src/com/tav/Main.java
+++ src/com/tav/Main.java
@@ -3,7 +3,7 @@ package com.tav;
 import com.tav.logging.Logger;
 
 public class Main {
-    private static final Logger LOGGER = new Logger(Main.class);
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
         LOGGER.info("hello");
--- src/com/tav/logging/Logger.java
+++ src/com/tav/logging/Logger.java
@@ -2,11 +2,24 @@ package com.tav.logging;
 
 import java.io.*;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.tav.logging.LoggerConfig.writer;
 
 public class Logger {
-    public Logger(final Class<?> owner) {
+    private static final Map<Class<?>, Logger> loggers = new HashMap<>();
+
+    public static Logger getLogger(Class<?> sourceClass) {
+        Logger logger = loggers.get(sourceClass);
+        if (logger == null) {
+            logger = new Logger(sourceClass);
+            loggers.put(sourceClass, logger);
+        }
+        return logger;
+    }
+
+    private Logger(final Class<?> owner) {
         this.source = owner;
     }
 
